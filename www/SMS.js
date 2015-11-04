44bd0b637016b5ceadbdbbc87d4b229cfafc8a22
vikram_m <vikram.menon@photoninfotech.net>
2015-11-04 12:38:15 +0530

    modified sms.js

--- www/SMS.js
+++ www/SMS.js
@@ -1,1 +1,11 @@
-console.log('TEST');
+  var exec = require('cordova/exec');
+  
+  var SmsPlugin = function() {}; 
+ 
+ 
+  SmsPlugin.prototype.doSomething = function(successCallback,failureCallback) {
+    return exec(successCallback, failureCallback, 'IncomingSms', 'StopReception', []);
+  }
+  
+  var smsplugin = new SmsPlugin();
+  module.exports = smsplugin;
